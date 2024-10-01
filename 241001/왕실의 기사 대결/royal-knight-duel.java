84854291004f64950d0d024697c6721008782272
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2024-10-01 20:34:04 +0900

    Title: 왕실의 기사 대결, Time: 114ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 241001/왕실의 기사 대결/royal-knight-duel.java
+++ 241001/왕실의 기사 대결/royal-knight-duel.java
@@ -20,11 +20,6 @@ public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	static boolean isPossible(int r, int c, int w, int h) {
-		
-		return true;
-	}
-
 	static int count(int r, int c, int w, int h) {
 		int cnt = 0;
 		for (int i = r; i < r + h; i++) {
@@ -55,7 +50,7 @@ public class Main {
 	static class Knight {
 		int id, r, c, w, h, hp, total;
 
-		public Knight(int id, int r, int c, int w, int h, int hp, int total) {
+		public Knight(int id, int r, int c, int h, int w, int hp, int total) {
 			this.id = id;
 			this.r = r;
 			this.c = c;
@@ -65,42 +60,61 @@ public class Main {
 			this.total = total;
 		}
 	}
-	
+
 	static void move(int id, int d) {
 		Set<Integer> set = new HashSet<>();
 		Deque<Integer> q = new ArrayDeque<>();
 		int[] dmgs = new int[N];
 		q.add(id);
 		set.add(id);
-		while(!q.isEmpty()) {
+		while (!q.isEmpty()) {
 			Knight nk = knights[q.poll()];
-			if(dead[id]) continue;
+			if (dead[id])
+				continue;
 			int nx = nk.r + dx[d];
 			int ny = nk.c + dy[d];
-			if(isPossible(nx, ny, nk.w, nk.h)) {
-				int dmg = count(nx, ny, nk.w, nk.h);
-				dmgs[nk.id] += dmg;
-				for(int i = 0 ; i < N ; i++) {
-					if(set.contains(i)) continue;
-					Knight k = knights[i];
-					if(isPushed(nk, k)) {
-						q.add(i);
-						set.add(i);
+//			System.out.println(nk.id);
+			for(int i = nx ; i < nx + nk.h ; i ++) {
+				for(int j = ny ; j < ny + nk.w ; j++) {
+					if(map[i][j] == 2) {
+//						System.out.println((nk.id + 1) + ": " + i + ", " + j);
+						return;
+					}else if(map[i][j] == 1) {
+						dmgs[nk.id]++;
 					}
 				}
-			}else {
-				return;
 			}
-			
+			for (int i = 0; i < N; i++) {
+				if (set.contains(i))
+					continue;
+				Knight k = knights[i];
+				int r1 = nx;
+				int r2 = k.r;
+				int c1 = ny;
+				int c2 = k.c;
+				int w1 = nk.w;
+				int w2 = k.w;
+				int h1 = nk.h;
+				int h2 = k.h;
+				if (r1 + h1 - 1 >= r2 && c1 + w1 - 1 >= c2 && r2 + h2 - 1 >= r1 && c2 + w2 - 1 >= c1) {
+//					System.out.println((nk.id + 1) + ", " + (k.id + 1) + "겹ㅣㅊ");
+					q.add(i);
+					set.add(i);
+				}
+			}
+
 		}
 		dmgs[id] = 0;
-		for(int s : set) {
+//		System.out.println(Arrays.toString(dmgs));
+		for (int s : set) {
 			Knight k = knights[s];
+			knights[s] = new Knight(k.id, k.r + dx[d], k.c + dy[d], k.h, k.w, k.hp - dmgs[k.id], k.total + dmgs[k.id]);
 			k.r += dx[d];
 			k.c += dy[d];
 			k.hp -= dmgs[k.id];
 			k.total += dmgs[k.id];
-			if(k.hp <= 0) dead[k.id] = true;
+			if (k.hp <= 0)
+				dead[k.id] = true;
 		}
 	}
 
@@ -118,14 +132,14 @@ public class Main {
 		Q = Integer.parseInt(st.nextToken());
 		map = new int[L + 2][L + 2];
 		Arrays.fill(map[0], 2);
-		for (int i = 0; i < L; i++) {
+		Arrays.fill(map[L + 1], 2);
+		for (int i = 1; i < L + 1; i++) {
 			st = new StringTokenizer(br.readLine());
 			map[i][0] = 2;
 			map[i][L + 1] = 2;
 			for (int j = 1; j < L + 1; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
-			
 		}
 //		System.out.println(L + " " + N + " " + Q);
 //		for (int[] row : map) {
@@ -140,7 +154,7 @@ public class Main {
 			int h = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
 			int k = Integer.parseInt(st.nextToken());
-			knights[i] = new Knight(i, r, c, w, h, k, 0);
+			knights[i] = new Knight(i, r, c, h, w, k, 0);
 		}
 //		System.out.println("start");
 //		for (Knight k : knights) {
@@ -154,7 +168,12 @@ public class Main {
 		for (int[] cmd : cmds) {
 			int kid = cmd[0] - 1;
 			int d = cmd[1];
+//			System.out.println("##############333"+(kid + 1) + "move " + d);
 			move(kid, d);
+//			for (Knight k : knights) {
+//				System.out.println((k.id + 1) + ": " + k.r + "," + k.c + " : hp: " + k.hp + ", totaldmg:  " + k.total);
+//			}
+//			System.out.println();
 		}
 		int ans = 0;
 		for (Knight k : knights) {

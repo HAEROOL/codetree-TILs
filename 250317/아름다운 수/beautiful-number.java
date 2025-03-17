df7e51b174a4bbd273c61a3ec47820e0a601e3a6
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2025-03-17 23:12:29 +0900

    Title: 아름다운 수, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250317/아름다운 수/beautiful-number.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    static void combination(int x, int[] sel){
+//    	System.out.println(x);
+        if(x == sel.length){
+            cnt++;
+            return;
+        }
+        for(int i = 1 ; i <= 9 ; i++) {
+        	if(x + i > k) continue;
+        	for(int j = x ; j < x + i ; j++) {
+        		sel[j] = i;
+        	}
+        	combination(x + i, sel);
+        }
+           
+    }
+    static int k;
+    static int cnt = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        k = sc.nextInt();
+        // Please write your code here.
+        combination(0, new int[k]);
+        System.out.println(cnt);
+    }
+}

dc4942315dfce1dc85c1dfb1d6311f690772554b
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2025-03-19 20:22:00 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 192ms, Memory: 15MB, Status: Passed - CodeTree

--- 250319/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.js
+++ 250319/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.js
@@ -11,21 +11,18 @@ const v = new Array(n).fill(null).map(() => new Array(m).fill(false));
 let isExit = false;
 function dfs(x, y){
     if(isExit) return;
-
     if(x === n - 1 && y === m - 1){
         isExit = true
         return;
     }
-
     for(let i = 0 ; i < 2 ; i++){
         const nx = x + dx[i];
         const ny = y + dy[i];
         if(nx >= n || nx < 0 || ny >= m || ny < 0) continue;
         if(!v[nx][ny] && grid[nx][ny] === 1){
             v[nx][ny] = true;
-
             dfs(nx, ny);
-            v[nx][ny] = false;
+            // v[nx][ny] = false;
         }
     }
 }

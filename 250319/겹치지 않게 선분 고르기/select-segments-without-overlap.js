fc8494d7777be1fbafd4a98db178c74cbc5a7d30
Haeroo Lee <71335694+HAEROOL@users.noreply.github.com>
2025-03-19 19:41:32 +0900

    Title: 겹치지 않게 선분 고르기, Time: 164ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250319/겹치지 않게 선분 고르기/select-segments-without-overlap.js
+++ 250319/겹치지 않게 선분 고르기/select-segments-without-overlap.js
@@ -4,48 +4,45 @@ const n = Number(input[0]);
 const segments = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
 
 let ans = Number.MIN_VALUE
-function dfs(depth, cnt, sel, v){
-    if(depth == n){
-        ans = Math.max(ans, cnt);
-        return;
-    }
-    for(let i = 0 ; i < n ; i++){
-        if(!v[i]){
+
+function cal(sel){
+    const v = new Array(1000).fill(false);
+    let cnt = 0;
+    for(let i = 0 ; i < sel.length ; i++){
+        if(sel[i]){
             let isPossible = true;
             const st = segments[i][0];
             const end = segments[i][1];
             for(let j = st ; j < end ; j++){
-                if(sel[j]){
+                if(v[j]){
                     isPossible = false;
                     break;
                 }
             }
             if(isPossible){
-                const tmp = [...sel];
                 for(let j = st ; j < end ; j++){
-                    tmp[j] = true;
+                    v[j] = true;
                 }
-                v[i] = true;
-                dfs(depth + 1, cnt + 1, tmp, v);
-                v[i] = false;
+                cnt++;
             }
-
         }
-        dfs(depth + 1, cnt, sel, v);
     }
+    ans = Math.max(cnt, ans);
 }
 
-for(let i = 0 ; i < n ; i++){
-    const v = new Array(n).fill(false)
-    const sel = new Array(1000).fill(false)
-    const st = segments[i][0];
-    const end = segments[i][1];
-
-    for(let j = st ; j < end + 1 ; j++){
-        sel[j] = true;
+function subset(k, sel){
+    if(k == sel.length){
+        cal(sel);
+        return;
     }
-    v[i] = true;
-    dfs(i, 1, sel, v)
+    sel[k] = true;
+    subset(k + 1, sel);
+
+    sel[k] = false;
+    subset(k + 1, sel)
+
 }
+
+subset(0, new Array(n).fill(false))
 
 console.log(ans)
